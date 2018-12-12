ab646a66ff4a89c2414e052f92edea703480b033
Hendel Cuartas <hendel@foostudio.mx>
2018-12-12 13:25:06 -0600

    feat(components/HendelLogo): component created and templated

---
+++ src/components/HendelLogo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/components/HendelLogo.styl';
+
+class HendelLogo extends React.Component {
+    render() {
+        return (
+            <p className="HendelLogo">HÉNDEL</p>
+        );
+    }
+}
+
+export default HendelLogo;
