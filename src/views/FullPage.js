13ab984349cf9df14e82842341ef3c90e123bc40
Hendel Cuartas <hendel@foostudio.mx>
2018-12-12 13:10:38 -0600

    feat(views/FullPage): component created and started template

---
+++ src/views/FullPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/views/FullPage.styl';
+
+class FullPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>HOLA MUNDO</h1>
+            </div>
+        );
+    }
+}
+
+export default FullPage;
