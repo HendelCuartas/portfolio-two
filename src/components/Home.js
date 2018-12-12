f556428b09184057c19bad10db0cb8e79721c7b8
Hendel Cuartas <hendel@foostudio.mx>
2018-12-12 15:46:30 -0600

    feat(components/Home): add content and style component

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/components/Home.styl';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="Home">
+                <div className="Home-container">
+                    <h1 className="Home-title">Creative Developer</h1>
+                    <p className="Home-subtitle">
+                        Hi, I’m
+                        <span className="Home-subtitleBold"> Héndel Cuartas</span>
+                        , an Interactive Media Designer and Developer based in
+                        <span className="Home-subtitleBold"> Mexico City.</span>
+                        {"\n"} Currently working as a Front End Developer at
+                        <span className="Home-subtitleBold"> Foo Studio </span>
+                        , a creative agency.
+                    </p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
