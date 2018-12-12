a3fdd404db65fa6b31bf3cdb9c0286a1304c72f8
Hendel Cuartas <hendel@foostudio.mx>
2018-12-12 15:45:39 -0600

    feat(components/Background): component created and templated

---
+++ src/components/Background.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/components/Background.styl';
+
+class Background extends React.Component {
+    render() {
+        return (
+            <div className="Background"></div>
+        );
+    }
+}
+
+export default Background;
