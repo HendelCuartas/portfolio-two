3ca3434b8460a3b8e6f722234ba67b3053f704a5
Hendel Cuartas <hendel@foostudio.mx>
2018-12-12 13:24:48 -0600

    feat(index.js): import FullPage and grid

--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FullPage from './views/FullPage';
 import './styles/index.styl';
 import './styles/utils/fonts.styl';
+import './styles/utils/grid.styl';
 import "gsap";
-import FullPage from './views/FullPage';
 
 class App extends React.Component {
-    constructor(props){
-        super(props);
-        // logo container
-        this.title = null;
-    }
-
-    componentDidMount(){
-        TweenMax.fromTo(this.title, 0.5, {opacity: 0, y: -40}, {opacity: 1, y: 0, ease: Power2.easeIn})
-    }
-
     render() {
         return (
             <div className="Home">
-                <h1 ref={ h1 => this.title = h1 }>Hello there!</h1>
                 <FullPage></FullPage>
             </div>
         );
