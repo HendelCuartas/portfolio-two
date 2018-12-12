68f4c567de3e6eaf799b3a435cea14399aa8a7fe
Hendel Cuartas <hendel@foostudio.mx>
2018-12-12 15:47:58 -0600

    feat(views/FullPage): import and use of Home component

--- src/views/FullPage.js
+++ src/views/FullPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../styles/views/FullPage.styl';
 import HendelLogo from '../components/HendelLogo';
+import Home from '../components/Home';
 
 class FullPage extends React.Component {
     render() {
         return (
-            <div>
+            <div className="FullPage">
+                <Home></Home>
                 <HendelLogo></HendelLogo>
             </div>
         );
