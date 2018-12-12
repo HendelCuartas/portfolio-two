e56d67a35521eaa8fcb0d97bae8278a95adee653
Hendel Cuartas <hendel@foostudio.mx>
2018-12-12 15:49:14 -0600

    feat(index.js): import and use of Background component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import FullPage from './views/FullPage';
+import Background from './components/Background';
 import './styles/index.styl';
 import './styles/utils/fonts.styl';
 import './styles/utils/grid.styl';
+import './styles/utils/utils.styl';
 import "gsap";
 
 class App extends React.Component {
     render() {
         return (
-            <div className="Home">
+            <div className="App">
+                <Background></Background>
                 <FullPage></FullPage>
             </div>
         );
